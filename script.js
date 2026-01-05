f0690cfb5ee333f20522270a2c5eec744f67da97
Tobi_03 <m4sk3d.t0bi@gmail.com>
2026-01-05 23:45:52 +0530

    made changes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const textarea = document.querySelector('textarea');
+const textareas = document.querySelectorAll("textarea");
 
 function autoResize() {
-  this.style.height = 'auto';
-  this.style.height = this.scrollHeight + 'px';
+  this.style.height = "auto";
+  this.style.height = this.scrollHeight + "px";
 }
 
-textarea.addEventListener('input', autoResize);
-
-// Call once on load to set initial height
-autoResize.call(textarea);
+textareas.forEach(t => {
+  t.addEventListener("input", autoResize);
+  autoResize.call(t);   // set initial height
+});
